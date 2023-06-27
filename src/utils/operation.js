6ca1e8bbed9f177a1cefaddb9ca66b6da474a79f
tarang0 <tarangmadhur@gmail.com>
2023-06-27 20:15:37 +0530

    Buy Ticket button functional End button left

--- src/utils/operation.js
+++ src/utils/operation.js
@@ -4,12 +4,14 @@ import { tezos } from "./tezos";
 
 export const buyTicketOperation = async () => {
     try{
-        const contract=await tezos.wallet.at("KT1F8k5puUD92QFMovr81Vn6AeZiiL6Eofq6");
+        // const contract=await tezos.wallet.at("KT1F8k5puUD92QFMovr81Vn6AeZiiL6Eofq6");
+        const contract=await tezos.wallet.at("KT1AApoNsbyXpWiHaeAF3wdbML1WVSFub8ea");
         const op =await contract.methods.buy_ticket().send({
             amount:1,
             mutez:false,
         })
         await op.confirmation(1);
+        // 1 represents that it waits for atleast 1 block after the operation is confirmed
     }
     catch(err)
     {
--- src/utils/tzkt.js
+++ src/utils/tzkt.js
@@ -1,3 +1,11 @@
 // TODO 8 - Fetch storage of the Lottery by completing fetchStorage
-
-export const fetchStorage = async () => {};
+import axios from "axios";
+export const fetchStorage = async () => {
+    try{
+        const res=await axios.get("https://api.ghostnet.tzkt.io/v1/contracts/KT1AApoNsbyXpWiHaeAF3wdbML1WVSFub8ea/storage/")
+        return (await res).data
+        //use res.data if error
+    }catch(err){
+        throw err;
+    }
+};
